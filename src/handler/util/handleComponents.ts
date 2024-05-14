acf5994b5e55260396aa4342d776e4399b1debff
Lukas Baum <lukasbaum.dev@gmail.com>
2024-05-14 10:22:59 +0200

    fix: optimizations & readability changes

--- src/handler/util/handleComponents.ts
+++ src/handler/util/handleComponents.ts
@@ -7,14 +7,17 @@ import { componentsFolderName } from "../../config";
 import { ComponentModule, ComponentTypes } from "../types/Component";
 
 export async function registerComponents(client: DiscordClient): Promise<void> {
-    let componentPaths: string[] = await glob(`**/${componentsFolderName}/**/**/*.js`);
+    const componentPaths: string[] = await glob(`**/${componentsFolderName}/**/**/*.js`);
 
     for (const componentPath of componentPaths) {
         const importPath: string = `../..${componentPath.replace(/^dist[\\\/]|\\/g, "/")}`;
         try {
             const module: ComponentModule = (await import(importPath)).default;
-            if (module.id) client.components[module.type].set(module.id, module);
-            else if (module.group) client.components[module.type].set(module.group, module);
+            const { id, group, type } = module;
+
+            if (id || group) {
+                client.components[type].set(id || group!, module);
+            }
         } catch (err) {
             Logger.error(`Failed to load component at ${importPath}`, err);
         }
@@ -22,28 +25,48 @@ export async function registerComponents(client: DiscordClient): Promise<void> {
 }
 
 export async function handleComponents(interaction: Interaction): Promise<void> {
-    let customId: string = "";
-    if (interaction.isButton() || interaction.isAnySelectMenu()) {
-        customId = interaction.customId.includes("group=")
-            ? interaction.customId.split(";")[0].replace(/group=|;/g, "")
-            : interaction.customId;
+    const { componentId, customId } = getIds(interaction);
+    if (interaction.isButton() || interaction.isAnySelectMenu()) interaction.customId = customId;
 
-        interaction.customId = interaction.customId.includes("group=")
-            ? interaction.customId.split(";")[1]
-            : interaction.customId;
-    } else if (interaction.isModalSubmit()) customId = interaction.customId;
+    if (!componentId) {
+        return Logger.error("The component id could not be filtered.", interaction);
+    }
 
-    if (customId === "") return Logger.error("The component id could not be filtered.", interaction);
+    const component: ComponentModule | undefined = client.components[getComponentType(interaction)].get(componentId);
 
-    const component: ComponentModule | undefined = client.components[getComponentType(interaction)].get(customId);
-    if (!component) return Logger.error(`No component matching ${customId} was found.`);
-    if (interaction.isModalSubmit() && component.group) return Logger.error(`The parameter group in ${customId} is not allowed.`);
+    const errorIdMessage = `group=${componentId === customId ? "none": componentId} id:${customId}`;
+    if (!component) {
+        return Logger.error(`No component matching ${errorIdMessage} was found.`);
+    }
+
+    if (interaction.isModalSubmit() && component.group) {
+        return Logger.error(`The parameter group in ${errorIdMessage} is not allowed.`);
+    }
 
     try {
         await component.execute(interaction);
     } catch (err) {
-        return Logger.error(`Error executing component with id: ${customId}`, err);
+        return Logger.error(`Error executing component with id: ${errorIdMessage}`, err);
     }
+}
+
+function getIds(interaction: Interaction) {
+    let customId = "";
+    let componentId = "";
+
+    if (interaction.isButton() || interaction.isAnySelectMenu()) {
+        const [idPart, customPart] = interaction.customId.split(";");
+        componentId = idPart.includes("group=") ? idPart.replace(/group=|;/g, "") : idPart;
+        customId = customPart || idPart;
+    } else if (interaction.isModalSubmit()) {
+        componentId = interaction.customId;
+        customId = interaction.customId;
+    }
+
+    return {
+        componentId,
+        customId
+    };
 }
 
 function getComponentType(interaction: Interaction): ComponentTypes {
