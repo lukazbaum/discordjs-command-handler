a24188b631520d9ca6dbb7cb4ad62c8c8941f607
Lukas Baum <lukasbaum.dev@gmail.com>
2024-05-14 12:18:30 +0200

    feat: add comments and examples for deleting commands

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,9 @@ export const client: DiscordClient = new DiscordClient({
         // and global commands (deploy: true) can take up to one hour.
         deploy: true
     });
+    // Existing commands can be deleted like this:
+    // await client.deleteCommand("1239882465229668414", RegisterTypes.Guild)
+    // await client.deleteCommands(["1239882465229668414", "1239882465229668414"], RegisterTypes.Guild)
+    // await client.deleteAllCommands(RegisterTypes.Guild)
     await client.connect(process.env.CLIENT_TOKEN);
 })();
